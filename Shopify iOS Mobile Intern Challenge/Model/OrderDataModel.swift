54275107b20fb8f5aa5850110c961415a51b5480
Jason0705 <6jasonli6@gmail.com>
2018-07-14 18:26:32 -0700

    Change file structure to MVC. Create Data Model: OrderDataModel.

---
+++ Shopify iOS Mobile Intern Challenge/Model/OrderDataModel.swift
@@ -0,0 +1,19 @@
+//
+//  OrderDataModel.swift
+//  Shopify iOS Mobile Intern Challenge
+//
+//  Created by Jason Li on 2018-07-14.
+//  Copyright © 2018 Jason Li. All rights reserved.
+//
+
+import UIKit
+
+class OrderDataModel {
+    
+    var province : String = ""
+    var orderYear : String = ""
+    var orderNumber : String = ""
+    var customerName : String = ""
+    var totalPrice : String = ""
+    
+}
